fd151736e7640fdb8691c43c0973542e6cf61a69
progratammz <ahmedtamer1998@gmail.com>
2021-08-14 15:18:38 +0200

    added navigation routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {NavigationShellComponent} from './core/components/navigation-shell/navigation-shell.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'app',
+    component: NavigationShellComponent,
+    children: []
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
