fa1247d89c39235415cc6c2e3d4b2287a87de93e
progratammz <ahmedtamer1998@gmail.com>
2021-08-14 15:15:06 +0200

    added custom colors in tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,38 @@
+const colors = require("tailwindcss/colors");
 module.exports = {
-    prefix: '',
-    purge: {
-      enabled: true,
-      content: [
-        './src/**/*.{html,ts}',
-      ]
+  prefix: "",
+  important: true,
+  purge: {
+    enabled: false,
+    content: ["./src/**/*.{html,ts}"],
+  },
+  darkMode: "class", // or 'media' or 'class'
+  theme: {
+    colors: {
+      gray: {
+        50: "#FCFCFC",
+        100: "#FCFCFC",
+        200: "#F7F7FC",
+        300: "#EFF0F6",
+        400: "#D9DBE9",
+        500: "#A0A3BD",
+        600: "#6E7191",
+        700: "#4E4B66",
+        800: "#262338",
+        900: "#14142A",
+      },
+      transparent: "transparent",
+      current: "currentColor",
+      black: colors.black,
+      white: colors.white,
+      indigo: colors.indigo,
+      red: colors.rose,
+      yellow: colors.amber,
     },
-    darkMode: 'class', // or 'media' or 'class'
-    theme: {
-      extend: {},
-    },
-    variants: {
-      extend: {},
-    },
-    plugins: [],
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
 };
