25d89742776de508d0c1c740c941e44874f5f11c
progratammz <ahmedtamer1998@gmail.com>
2021-08-18 17:31:39 +0200

    added product pages to imports

--- src/app/pages/products/products.module.ts
+++ src/app/pages/products/products.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ProductsRoutingModule } from './products-routing.module';
+import { ProductsListPage } from './products-list/products-list.page';
+import { ProductsManipulatePage } from './products-manipulate/products-manipulate.page';
+import { ProductsDetailsPage } from './products-details/products-details.page';
 
 @NgModule({
-  declarations: [],
+  declarations: [ProductsListPage, ProductsManipulatePage, ProductsDetailsPage],
   imports: [CommonModule, ProductsRoutingModule],
 })
 export class ProductsModule {}
