bc0e1009d461c0ca7249232031d26c5782c780e5
progratammz <ahmedtamer1998@gmail.com>
2021-08-19 10:45:02 +0200

    added auth routes

--- src/app/pages/auth/auth-routing.module.ts
+++ src/app/pages/auth/auth-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ForgetPasswordPage } from './forget-password/forget-password.page';
+import { LoginPage } from './login/login.page';
+import { RegisterPage } from './register/register.page';
+import { SetupPage } from './setup/setup.page';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginPage, data: { animation: 0 } },
+  { path: 'logout', component: LoginPage, data: { animation: 0 } },
+  { path: 'register', component: RegisterPage, data: { animation: 1 } },
+  {
+    path: 'forgot-password',
+    component: ForgetPasswordPage,
+    data: { animation: 2 },
+  },
+  { path: 'setup', component: SetupPage, data: { animation: 2 } },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AuthRoutingModule { }
+export class AuthRoutingModule {}
