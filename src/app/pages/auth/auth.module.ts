8bc86cabddbcb3e0017197e8ded846f79432385a
progratammz <ahmedtamer1998@gmail.com>
2021-08-19 10:44:37 +0200

    added setup page to auth module

--- src/app/pages/auth/auth.module.ts
+++ src/app/pages/auth/auth.module.ts
@@ -5,13 +5,15 @@ import { AuthRoutingModule } from './auth-routing.module';
 import { LoginPage } from './login/login.page';
 import { RegisterPage } from './register/register.page';
 import { ForgetPasswordPage } from './forget-password/forget-password.page';
+import { SetupPage } from './setup/setup.page';
 
 
 @NgModule({
   declarations: [
     LoginPage,
     RegisterPage,
-    ForgetPasswordPage
+    ForgetPasswordPage,
+    SetupPage
   ],
   imports: [
     CommonModule,
