1b381593fbe9bd41859c6d2e15c56afa84c83dd5
progratammz <ahmedtamer1998@gmail.com>
2021-08-19 06:46:40 +0200

    added new pages to auth modules

--- src/app/pages/auth/auth.module.ts
+++ src/app/pages/auth/auth.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
+import { LoginPage } from './login/login.page';
+import { RegisterPage } from './register/register.page';
+import { ForgetPasswordPage } from './forget-password/forget-password.page';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginPage,
+    RegisterPage,
+    ForgetPasswordPage
+  ],
   imports: [
     CommonModule,
     AuthRoutingModule
